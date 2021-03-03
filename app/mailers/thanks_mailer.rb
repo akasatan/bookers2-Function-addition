0f4fb29131da054eaaa7619aed7b1d70ee8b026c
akasatan <aka435304@gmail.com>
2021-03-03 14:57:09 +0000

    mail機能エラー中修正前

--- app/mailers/thanks_mailer.rb
+++ app/mailers/thanks_mailer.rb
@@ -1,2 +1,9 @@
-class ThanksMailer < ApplicationMailer
+class ThanksMailer < Devise::Mailer
+    default :from => ENV['USER_NAME']
+
+    def send_signup_email(user)
+        @user = user
+        @greeting = "@greetingの部分だよ～"
+        mail( :to => "@user.email", :subject => "会員登録が完了しました。" )
+    end
 end
