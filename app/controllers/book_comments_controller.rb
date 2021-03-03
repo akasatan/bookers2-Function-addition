1e94d23c91f53ece3df903f851277d7907a0f9ea
akasatan <aka435304@gmail.com>
2021-03-03 07:27:03 +0000

    [fix]ajax非同期実装解答修正

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,20 +1,20 @@
 class BookCommentsController < ApplicationController
   before_action :ensure_correct_user
+  before_action :authenticate_user!
 
   def create
     @book_comment = current_user.book_comments.new(book_comment_params)
-    if @book_comment.save
-      @book_comments = @book.book_comments
-    else
+    @book_comments = @book.book_comments
+    unless @book_comment.save
       @newbook = Book.new
-      @book_comments = @book.book_comments
+      render 'error'
     end
   end
 
   def destroy
     @book_comment = BookComment.find_by(id: params[:id], book_id: @book)
+    @book_comments = @book.book_comments
     if @book_comment.destroy
-      @book_comments = @book.book_comments
     end
   end
 
