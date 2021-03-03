1f80631d0cbf9767267d13575636d227db68af7d
akasatan <aka435304@gmail.com>
2021-03-03 06:24:57 +0000

    [add]ajax非同期通信実装

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,20 +4,17 @@ class BookCommentsController < ApplicationController
   def create
     @book_comment = current_user.book_comments.new(book_comment_params)
     if @book_comment.save
-      redirect_to book_path(@book), notice: "You have created comment successfully."
+      @book_comments = @book.book_comments
     else
       @newbook = Book.new
       @book_comments = @book.book_comments
-      render template: 'books/show'
     end
   end
 
   def destroy
     @book_comment = BookComment.find_by(id: params[:id], book_id: @book)
     if @book_comment.destroy
-      redirect_to book_path(@book), notice: "You have delete comment successfully."
-    else
-      render template: 'books/show'
+      @book_comments = @book.book_comments
     end
   end
 
